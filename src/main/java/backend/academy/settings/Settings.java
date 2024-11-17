02e8673a2af28d32dc8cdaee38b98489730e67e4
Almaz <almaz.kzn2005@yandex.ru>
2024-11-17 21:11:17 +0300

    feat: add Settings class

---
+++ src/main/java/backend/academy/settings/Settings.java
@@ -0,0 +1,12 @@
+package backend.academy.settings;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Settings {
+    public static final int PERCENTILE_95 = 95;
+    public static final String RESULT_MARKDOWN_PATH = "src/main/result.md";
+    public static final String RESULT_ADOC_PATH = "src/main/result.adoc";
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+}
