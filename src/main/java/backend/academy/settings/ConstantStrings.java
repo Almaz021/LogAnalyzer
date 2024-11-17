f7fd9737cbb294795f46c700e5d861555dcb22bf
Almaz <almaz.kzn2005@yandex.ru>
2024-11-17 21:08:27 +0300

    feat: add ConstantStrings class

---
+++ src/main/java/backend/academy/settings/ConstantStrings.java
@@ -0,0 +1,31 @@
+package backend.academy.settings;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ConstantStrings {
+    public static final String MAIN_INFO = " Общая информация";
+    public static final String ADOC_TABLE_START = "[options=\"header\"]";
+    public static final String FIRST_DELIMITER = "=";
+    public static final String SECOND_DELIMITER = "|";
+    public static final String NEW_LINE = "\n";
+    public static final String METRIC = "Метрика";
+    public static final String VALUE = "Значение";
+    public static final String FILES = "Файл(-ы)";
+    public static final String START_DATE = "Начальная дата";
+    public static final String END_DATE = "Конечная дата";
+    public static final String REQUEST_COUNT = "Количество запросов";
+    public static final String AVG_RESPONSE_SIZE = "Средний размер ответа";
+    public static final String PERCENTILE_95_TEXT = "95p размера ответа";
+    public static final String HEADER_SEPARATOR = "|===";
+    public static final String REQUESTED_RESOURCES = " Запрашиваемые ресурсы";
+    public static final String RESOURCE_COLUMN = "Ресурс";
+    public static final String COUNT_COLUMN = "Количество";
+    public static final String RESPONSE_CODES = " Коды ответа";
+    public static final String CODE_COLUMN = "Код";
+    public static final String NAME_COLUMN = "Имя";
+    public static final String TABLE_HEADER_SEPARATOR = "|:---------------------:|-------------:|";
+    public static final String TABLE_ROW_SEPARATOR = "|:---:|:---------------------:|-----------:|";
+    public static final String MARKDOWN_HASH = "####";
+    public static final String NA = "N/A";
+}
