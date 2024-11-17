0d2ef616ded50bb5dd6c27c8ae356bc7c56f49ee
Almaz <almaz.kzn2005@yandex.ru>
2024-11-17 21:10:13 +0300

    feat: change Main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,21 @@
 package backend.academy;
 
+import backend.academy.services.ExceptionHandlerService;
+import backend.academy.services.StartService;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        try {
+            StartService startService =
+                new StartService(args, new PrintWriter(System.out, true, StandardCharsets.UTF_8));
+            startService.start();
+        } catch (Exception e) {
+            ExceptionHandlerService.handleException(e);
+            System.exit(1);
+        }
     }
 }
