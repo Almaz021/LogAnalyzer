2411cfe6c8399f25bf1eaaa5b5fd8ef315371c16
Almaz <almaz.kzn2005@yandex.ru>
2024-11-17 21:09:49 +0300

    feat: change LogRecord class

--- src/main/java/backend/academy/entities/LogRecord.java
+++ src/main/java/backend/academy/entities/LogRecord.java
@@ -1,13 +1,14 @@
 package backend.academy.entities;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record LogRecord(String remoteAddr,
                         String remoteUser,
                         LocalDate timeLocal,
-                        String request,
+                        String[] request,
                         Integer status,
-                        Integer bodyBytesSent,
+                        BigDecimal bodyBytesSent,
                         String httpReferer,
                         String httpUserAgent) {
 }
