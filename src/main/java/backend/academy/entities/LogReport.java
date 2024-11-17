17ff22d09df401c3b813f0ae9566c51fba0252a3
Almaz <almaz.kzn2005@yandex.ru>
2024-11-17 21:09:58 +0300

    feat: change LogReport class

--- src/main/java/backend/academy/entities/LogReport.java
+++ src/main/java/backend/academy/entities/LogReport.java
@@ -1,6 +1,34 @@
 package backend.academy.entities;
 
+import com.google.common.math.Quantiles;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+import static backend.academy.settings.Settings.PERCENTILE_95;
 
-public record LogReport(List<LogRecord> records) {
+@Getter
+@Setter
+public class LogReport {
+    private List<String> files = new ArrayList<>();
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Integer requestCount = 0;
+    private BigDecimal sumRequestSize = BigDecimal.ZERO;
+    private List<BigDecimal> allRequestSize = new ArrayList<>();
+    private Map<String, Integer> resourcesCount = new HashMap<>();
+    private Map<String, Integer> requestStatusCount = new HashMap<>();
+
+    public BigDecimal getAverageRequestSize() {
+        return sumRequestSize.divide(new BigDecimal(requestCount), RoundingMode.FLOOR);
+    }
+
+    public Double getPercentile() {
+        return Quantiles.percentiles().index(PERCENTILE_95).compute(allRequestSize);
+    }
 }
