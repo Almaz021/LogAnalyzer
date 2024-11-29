b5cdb05bcd434ee96f82ab1c22ebe9e5d0cdcc4b
Almaz <almaz.kzn2005@yandex.ru>
2024-11-29 08:52:11 +0300

    refactor: mock URLReader in testReadURL to avoid network dependency

--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -19,9 +19,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
 
 class AllTests {
 
@@ -37,9 +39,14 @@ class AllTests {
 
     @Test
     public void testReadURL() throws IOException {
-        URLReader urlReader = new URLReader();
-        Stream<String> stream = urlReader.read(
-            "https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs");
+        URLReader mockUrlReader = Mockito.mock(URLReader.class);
+
+        Stream<String> mockedStream = Stream.of(
+            "93.180.71.3 - - [17/May/2015:08:05:32 +0000] \"GET /downloads/product_1 HTTP/1.1\" 304 0 \"-\" \"Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.21)\""
+        );
+        Mockito.when(mockUrlReader.read(anyString())).thenReturn(mockedStream);
+
+        Stream<String> stream = mockUrlReader.read("mocked_url");
 
         List<String> lines = stream.limit(1).toList();
 
